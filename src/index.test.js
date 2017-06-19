3ccee0b924feb52e4791dc9e6cc73734a829dc07
DeividZavala <crusedmoss@gmail.com>
2017-06-19 17:16:38 -0500

    entorno de desarrollo

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe("Primer test de prueba", () => {
+	it("deberia pasar", () => {
+		expect(true).toEqual(true);
+	});
+});
---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+/* eslint-disable no-console */
+
+console.log("Iniciando la app en modo desarrollo".green);
