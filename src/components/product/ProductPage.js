22a8a5d871d73511fc3beb4aaae03dc72852cbeb
DeividZavala <crusedmoss@gmail.com>
2017-06-22 17:34:37 -0500

    agregando componente del producto :octocat:

---
+++ src/components/product/ProductPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProductPage extends React.Component{
+
+	render(){
+		return(
+			<div>
+				<h1>Pagina de producto</h1>
+			</div>
+		);
+	}
+
+}
+
+export default ProductPage;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import AboutPage from './components/about/AboutPage';
 import HomePage from './components/home/HomePage';
+import ProductPage from './components/product/ProductPage';
 
 
 export default (
 
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage}/>
+		<Route path="products" component={ProductPage} />
 		<Route path="about" component={AboutPage}/>
 	</Route>
 
